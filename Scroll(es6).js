6662a7099cb282ccd305ca5f4e1a37a8186b5117
NeedAChange <13286904669@163.com>
2016-11-01 19:30:27 +0800

    add es6 version

---
+++ Scroll(es6).js
@@ -0,0 +1,24 @@
+class Scroll {
+  constructor() {
+
+  },
+  init(){
+    console.log('good');
+  },
+  move(){
+
+  },
+  autoMove(){
+
+  },
+  arrow(){
+
+  },
+  tap(){
+
+  },
+  current(){
+
+  }
+}
+export default class { Scroll }
